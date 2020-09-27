4a499130ac2a9f1db824cd0d331f14137776ba51
dan <43108050+dumbkiddo@users.noreply.github.com>
2020-09-27 17:26:00 +0600

    Added files

---
+++ Assets/Scripts/CanvasButtons.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasButtons : MonoBehaviour
+{
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
